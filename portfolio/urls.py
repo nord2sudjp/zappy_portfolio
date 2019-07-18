b2315abd04ec6656fcea9c75893a5a3031b818d9
maka y <nord2sudjp@gmail.com>
2019-07-18 11:48:27 +0900

    penpo

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,10 +5,10 @@ from django.conf.urls.static import static
 import jobs.views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', jobs.views.home, name='home'),
-    path('job', jobs.views.home, name='jobs.urlhome'),
-    path('blog/', include('blog.urls')),
+    path('penpo/admin/', admin.site.urls),
+    path('penpo/', jobs.views.home, name='home'),
+    path('penpo/job/', jobs.views.home, name='jobs.urlhome'),
+    path('penpo/blog/', include('blog.urls')),
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
       + static(settings.CSS_URL, document_root=settings.CSS_ROOT) \
       + static(settings.SCRIPT_URL, document_root=settings.SCRIPT_ROOT)
